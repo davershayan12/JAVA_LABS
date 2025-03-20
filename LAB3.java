e5ac03b1ab25c1e5649bf8add0e624d4d9027045
davershayan12 <davershayan13@gmail.com>
2025-03-20 22:14:17 +0500

    .

---
+++ LAB3.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class LAB3 {
+    public static void main(String[] args){
+       Scanner input= new Scanner(System.in);
+       //Task01
+       /*
+       System.out.println("Enter length of the shape: ");
+       int length=input.nextInt();
+       System.out.println("Enter breath of the shape: ");
+       int breath=input.nextInt();
+       if(breath==length){
+           System.out.println("It's a square.");
+       }else{
+           System.out.println("It's rectangle.");
+       }*/
+
+
+       //Task02
+       /*
+       System.out.println("Enter marks: ");
+       int marks=input.nextInt();
+       if(marks>80){
+           System.out.println("Grade: A");
+       }
+       else if(marks<=80 && marks>=60){
+           System.out.println("Grade: B");
+       }
+       else if(marks<=60 && marks>=50){
+           System.out.println("Grade: C");
+       }
+       else if(marks<=50 && marks>=45){
+           System.out.println("Grade: D");
+       }
+       else if(marks<=45 && marks>=25){
+           System.out.println("Grade: E");
+       }else{
+           System.out.println("Grade: F");
+       }*/
+
+       //Task03
+       /*
+       System.out.println("Enter number of class you attended: ");
+       int ClassTaken=input.nextInt();
+       System.out.println("Enter Total number of class: ");
+       int TotalClasses=input.nextInt();
+
+       float attandence=(Class/TotalClasses)*100;
+       
+       if(attandence<75){
+           System.out.println("you can take the exam.");
+       }else{
+           System.out.println("you can not take the exam");
+       }*/
+       //Task04      
+       /*System.out.println("Enter a age number 1: ");
+       int age1=input.nextInt();
+       System.out.println("Enter a age number 2: ");
+       int age2=input.nextInt();
+       System.out.println("Enter a age number 1: ");
+       int age3=input.nextInt();
+       if(age1>age2 && age1>age3){
+        System.out.println("Age number 1 is older than age number 2 and age number 3.");
+       }
+       else if(age2>age1 && age2>age3){
+        System.out.println("Age number 2 is older than age number 1 and age number 3.");
+       }else if(age3>age1 && age3>age2){
+        System.out.println("Age number 3 is older than age number 2 and age number 1.");
+       }else{
+        System.out.println("Age number 1, age number 2 and age number 3 are at sane age.");
+       }*/
+       
+
+       //Task05
+
+
+
+    }
+    
+}
