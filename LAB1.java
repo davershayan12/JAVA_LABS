20bf5fb1db3f4f966663ae82a46c01a4896eaead
davershayan12 <davershayan13@gmail.com>
2025-03-20 21:36:32 +0500

    ,

--- LAB1.java
+++ LAB1.java
@@ -47,6 +47,44 @@ public class LAB1{
 
 
         //task4
+        /*System.out.println("Enter weight :");
+        int weight = input.nextInt();
+        System.out.println("Enter height(inches) :");
+        int height = input.nextInt();
+
+        double kilogram = weight * 0.45359237;
+        double feet = height * 0.0254;
+        double Bmi = kilogram / (feet * feet);
+
+        System.out.println("the BMI is " + Bmi);*/
+
+
+        //task5
+        /*
+         System.out.println("Enter numbers 01:");
+        int num1 = input.nextInt();
+        System.out.println("Enter numbers 02:");
+        int num2 = input.nextInt();
         
+        int sum = a + b;
+        int difference = a - b;
+        
+        System.out.println("Sum: " + sum + " Difference: " + difference);
+         */
+        
+
+        //task6
+        /*
+        System.out.println("Enter three digit number:");
+
+        int num = input.nextInt();
+
+        int ones = num % 10;
+        int ten = (num % 100) / 10;
+        int hundreds = num / 100;
+        int sum = ones + ten + hundreds;
+
+        System.out.println("sum of " + num + " is " + sum);
+         */
         }
 }
