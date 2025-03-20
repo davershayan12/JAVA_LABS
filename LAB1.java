0c64ca2d041f6b3912ef1c5cdfd0b52e6555c91e
davershayan12 <davershayan13@gmail.com>
2025-03-20 21:27:30 +0500

    .

---
+++ LAB1.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class LAB1{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        
+        
+        //task1
+        /*System.out.println("Enter your value in inchs:");
+        int inch = input.nextInt();
+        double meter = inch * 0.0254;
+        System.out.println("In meter:" + meter);
+        */
+        
+        
+        //task2
+        /*System.out.println("Enter your name:");
+        String name = input.next();
+        System.out.println("Enter your father's name:");
+        String fname = input.next();
+        System.out.println("Enter your age:");
+        String age = input.next();
+        System.out.println("Enter your gender:");
+        String gender = input.next();
+        System.out.println("Enter your department:");
+        String dep = input.next();
+        System.out.println("Enter your email:");
+        String email = input.next();
+        
+        
+        System.out.println("Hello " + name + " nice to meet you.");
+        System.out.println("Name : " +name);
+        System.out.println("Father's Name : " +fname);
+        System.out.println("Age : " +age);
+        System.out.println("Gender : " +gender);
+        System.out.println("Department : " +dep);
+        System.out.println("Name : " +name);
+        System.out.println("Email : " +email);*/
+        
+        
+        //task3
+        /*System.out.println("Input the number of days: ");
+        int days = input.nextInt();
+        int Days = days % 30; // Corrected: Use modulo to get remaining days
+        int Months = days / 30;
+        System.out.println(days + " days are " + Months + " month and " + Days + " days.");
+        */
+
+
+        //task4
+        
+        }
+}
