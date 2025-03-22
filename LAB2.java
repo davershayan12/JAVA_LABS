0624ef54f1532a1808ebf104b6f89b02e8534f8c
davershayan12 <davershayan13@gmail.com>
2025-03-22 11:56:55 +0500

    l

---
+++ LAB2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class LAB2 {
+    public static void main (String[] args){
+        //Task01
+        /*Create a Java application for a basic calculator with the following capabilities:
+        Addition and Subtraction
+        Division, Exponentiation, and Multiplication
+        */
+        Scanner input= new Scanner(System.in);
+        int num1=input.nextInt();
+        int num2=input.nextInt();
+        char operator=input.next().chars(0);
+
+
+    }
+    
+}
