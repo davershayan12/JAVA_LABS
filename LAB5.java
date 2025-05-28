28150f50fb12227cc0afe234f43ef17a83d48b17
davershayan12 <davershayan13@gmail.com>
2025-05-28 15:08:12 +0500

    .

---
+++ LAB5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LAB5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("hi");
+        /*A.	Write a Java program to search for a specific character in a given string. If found, print "Character found at index" followed by its index. If not found, print "Character not found”. */
+
+        /*System.out.println("Enter sentences:");
+        String sentences = in.nextLine();
+        System.out.println("Enter search:");
+        char search = in.next().charAt(0);
+
+        if (sentences.indexOf(search) == -1) {
+            System.out.println(search + " doesn't exist in the sentences.");
+        } else {
+            System.out.println(search + " exists at the index " + sentences.indexOf(search));
+        }*/
+    }
+}
